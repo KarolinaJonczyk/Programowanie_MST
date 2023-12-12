84722300a051460ab944b70cab0d081256b5e8a8
Karolina Jonczyk <96690424+KarolinaJonczyk@users.noreply.github.com>
2023-12-12 11:49:53 +0100

    Update basevector.py
    Update docstrings in methods

--- basevector.py
+++ basevector.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Mar 13 21:53:14 2022
-
+Last update: Tue Dec 12 11:49:14 2023
 @author: karolinajonczyk
 """
 
@@ -11,65 +11,65 @@ import numpy as np
 
 class BaseVector():
     def __init__(self, size=None):
-        """Konstruktor"""
+        """Constructor"""
         self.size = size if size is not None else 3
         self.vector=np.ones(self.size,dtype=np.float64)
 
     def random(self):
-        """Metoda do losowej generacji elementów wektora """
+        """Method for randomly generating vector elements"""
         rand=np.random.randint(0,100,size=self.size)
         self.vector *= rand
         print(f'Wylosowano wektor: {self.vector}')
         return self.vector
 
     def overwrite(self, vector):
-        """Metoda do wczytywania elementów wektora z listy podanej jako argument"""
+        """Method for loading vector elements from a list provided as an argument"""
         self.vector=vector
         print(f'Zapisano wektor: {self.vector}')
         return self.vector
 
     def __add__(self, other):
-        """Operator dodawania dwóch wektorów"""
+        """Operator adding two vectors"""
         return np.add(self.vector , other.vector)
     
     def __sub__(self, other):
-        """Operator odejmowania dwóch wektorów"""
+        """Operator subtracting two vectors"""
         return np.subtract(self.vector , other.vector)
 
     def __mul__(self, scalar):
-        """Mnożenie wektora przez skalar"""
+        """Vector multiplication by scalar"""
         return  self.vector*scalar
 
     def size(self):
-        """Metoda sprawdzająca rozmiar"""
+        """Method checking the size of a vector"""
         return len(self.vector)
 
     def length(self):
-        """Metoda wyliczającą długość wektora"""
+        """Method calculating the length of a vector"""
         return np.linalg.norm(self.vector)
 
     def sum(self):
-        """Metoda wyliczającą sumę elementów wektora"""
+        """Method calculating the sum of vector elements"""
         return np.sum(self.vector)
 
     def __str__(self):
-        """Reprezentacja tekstowa wektora - generujemy stringa"""
+        """Text representation of a vector - generating a string"""
         return str(self.vector)
 
     def __repr__(self):
-        """Reprezentacja tekstowa wektora - możemy printować"""
+        """Text representation of a vector"""
         return print(self.vector)
 
     def scalar_product(self, other):
-        """Metoda wyliczającą iloczyn skalarny dwóch wektorów"""
+        """Method calculating the dot product of two vectors"""
         return np.dot(self.vector,other.vector)
 
     def __getitem__(self, a):
-        """Operator [] pozwalający na dostęp do konkretnych elementów wektora"""
+        """Operator [] allowing access to specific elements of a vector"""
         return self.vector[a]
 
     def __contains__(self, a):
-        """Operator in sprawdzający przynależność elementu do wektora."""
+        """Operator checking the inclusion of an element in a vector"""
         if a in self.vector:
             return True
         else:
