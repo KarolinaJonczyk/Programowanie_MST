dd48872416a4be81cc305f3e7a4dcc9ac5df24e8
Karolina Jonczyk <96690424+KarolinaJonczyk@users.noreply.github.com>
2023-12-12 11:55:17 +0100

    Update testvector.py
    variables change, pol -> eng changes

--- testvector.py
+++ testvector.py
@@ -9,29 +9,26 @@ Created on Sun Mar 13 21:55:58 2022
 import basevector
 import numpy as np
 
-#ustalmy ułatwienie do testowania: p - pierwszy wektor, d - drugi wektor
-p=basevector.BaseVector()
+vector1=basevector.BaseVector()
 
-d=basevector.BaseVector()
+vector2=basevector.BaseVector()
 
-#sprawdzamy wektor o rozmiarze 5
-print("sprawdzamy konkretny: ", basevector.BaseVector(5))
+print("check is vector length 5: ", basevector.BaseVector(5))
 
-#sprawdzamy czy wyrzuci nam wektor o rozmiarze 3, bo tak zakladamy domyślnie
-print("sprawdzamy domyślny: ", basevector.BaseVector())
+#expected: vector length 3
+print("check default vector: ", basevector.BaseVector())
 
-
-print("wektor pierwszy ma postać: ", p.random())
-print("wektor drugi ma postać: ", d.random())
-print("sprawdzamy overwrite: ", p.overwrite(np.array([2,1,-1])))
-print("test __add__ :", p.__add__(d))
-print("test __sub__ :", p.__sub__(d))
-print("test __mul__ :", p.__mul__(7))
-print("test size:", p.size)
-print("test length:", p.length())
-print("test sum:", p.sum())
-print("test __str__:", p.__str__())
-print("test __repr__:", p.__repr__())
-print("test scalar_product:", p.scalar_product(d))
-print("test __getitem__:", p.__getitem__(p[2])) 
-print("test __contains__:", p.__contains__(p[0]))
+print("first vector is: ", vector1.random())
+print("second vector is: ", vector2.random())
+print("check overwrite: ", vector1.overwrite(np.array([2,1,-1])))
+print("test __add__ :", vector1.__add__(vector2))
+print("test __sub__ :", vector2.__sub__(vector2))
+print("test __mul__ :", vector1.__mul__(7))
+print("test size:", vector1.size)
+print("test length:", vector1.length())
+print("test sum:", vector1.sum())
+print("test __str__:", vector1.__str__())
+print("test __repr__:", vector1.__repr__())
+print("test scalar_product:", vector1.scalar_product(vector2))
+print("test __getitem__:", vector1.__getitem__(vector1[2])) 
+print("test __contains__:", vector1.__contains__(vector1[0]))
